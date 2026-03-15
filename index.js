545e2025597da3b17eb4687dc6cf4a31b3eddde2
phtav <pedro.idk677@gmail.com>
2026-03-15 18:36:37 -0300

    Projeto estruturado e limpo

---
+++ index.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+const pool = new Pool ({
+user: process.env.DB_USER,
+host: process.env.DB_HOST,
+database: process.env.DB_NAME,
+password: process.env.DB_PASS,
+port: process.env.DB_PORT,
+});
+
+
+async function inserirFornecedor(razao_social, CNPJ, email) {
+try {
+	const sql = "INSERT INTO fornecedores (razao_social, CNPJ, email) VALUES ($1, $2, $3) RETURNING *";
+	const valores = [razao_social, CNPJ, email];
+	const resultado = await pool.query(sql, valores);
+
+	console.log("Fornecedor cadastrado com ID:",resultado.rows[0].id_fornecedor);
+} catch (erro) {
+	console.error("Erro no banco: ", erro.message);
+}
+}
+
+async function inserirProdutos(nome, preco, estoque, id_fornecedor) {
+try {
+	const sql = 'INSERT INTO produtos (nome, preco, estoque, id_fornecedor) VALUES ($1, $2, $3, $4) RETURNING *';
+	const valores = [nome, preco, estoque, id_fornecedor];
+	const res = await pool.query(sql, valores);
+
+	console.log("Produto cadastrado com sucesso!");
+	console.table(res.rows);
+} catch (erro) {
+	console.error("Erro ao cadastrar produto:", erro.message);
+}
+}
+
+async function relatorioEstoque() {
+try {
+	const sql = 
+`SELECT 
+p.nome AS PRODUTOS,
+p.preco,
+p.estoque,
+f.razao_social AS fornecedor
+FROM produtos p
+INNER JOIN fornecedores f ON p.id_fornecedor = f.id_fornecedor
+`;
+
+	const res = await pool.query(sql);
+
+console.log("Relatório detalhado:");
+console.table(res.rows);
+} catch (erro) {
+console.error("Erro ao gerar relatório:", erro.message);
+}
+}
+
+relatorioEstoque();
